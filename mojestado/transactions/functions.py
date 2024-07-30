431af2e1ad972832a1f23bac6d4163f712d13afb
miiihaaas <miiihaaas@gmail.com>
2024-07-30 16:26:38 +0200

    added

--- mojestado/transactions/functions.py
+++ mojestado/transactions/functions.py
@@ -1,11 +1,30 @@
 import datetime
 
-from flask import session
+from flask import flash, json, redirect, session, url_for
 from mojestado import db
 from mojestado.models import Animal, Product, User
 
 
+def generate_payment_slips(fattening_list):
+    '''
+    generiše 2+ uplatnice na jednom dokumentu (A4) u fpdf, qr kod/api iz nbs
+    '''
+    print('generisane uplatnice na jednom dokumentu (A4). dokument će biti attachovan u email')
+    pass
+
+
+def generate_invoice():
+    '''
+    generiše fakturu. dokument će biti attachovan u emali.
+    '''
+    print('generisana faktura. dokument bi trebao biti attachovan u email')
+    pass
+
+
 def register_guest_user(form_object):
+    if not form_object.get('email'):
+        flash ('Niste uneli email', 'danger')
+        return redirect(url_for('transactions.guest_form'))
     user = User(email=form_object.get('email'),
                 name=form_object.get('name'),
                 surname=form_object.get('surname'),
@@ -24,14 +43,35 @@ def check_bank_balance() -> bool:
 
 
 def create_invoice():
+    '''
+    čuva podaatke u db (faktura i stavke)
+    '''
     print('wip: Faktura kreirana')
     pass
 
 
 def send_email(user, form_object):
+    '''
+    ako je na rate šalje fakturu i sve uplatnice
+    ako nije na rate šalje samo fakturu
+    '''
+    cart_data = json.loads(form_object.get('cartData'))
+    print(f'{cart_data=}')
+    fattening_list = cart_data['fattening']
+    na_rate = False
+    print(f'{fattening_list=}')
+    for fattening in fattening_list:
+        if int(fattening['br_rata']) > 1:
+            na_rate = True
+            break
+    if na_rate:
+        print('wip: faktura i uplatnice')
+        payment_slips = generate_payment_slips(fattening_list) # generiše 2+ uplatnice na jednom dokumentu (A4)
+    else:
+        print('wip: samo faktura')
+    invoice_file = generate_invoice()
     to = user.email
     subject = "Potvrda transakcije"
-    cart_data = form_object.get('cartData')
     body = f"Poštovani/a {user.name},\n\nVaša transakcija je uspešno izvršena.\n\nDetalji kupovine:\n{cart_data}\n\nHvala na poverenju!"
     
     print(f"To: {to}")
